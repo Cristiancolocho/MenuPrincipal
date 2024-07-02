6a650e5d3abd714a73f321d7b08a00224a259fe4
CHATO LOKO <colochocristian72@gmail.com>
2024-07-01 22:46:25 -0600

    Haciendo pruebas tecnicas, agregando pagina Cambio, agregando botones

---
+++ MenuPrincipal/Cambio.xaml.cs
@@ -0,0 +1,25 @@
+namespace MenuPrincipal;
+
+public partial class Cambio : ContentPage
+{
+	public Cambio()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(AAA1.Text) && !string.IsNullOrEmpty(AAA2.Text))
+        {
+            double resultado, moneya, moneyb;
+            moneya = Convert.ToDouble(AAA1.Text);
+            moneyb = Convert.ToDouble(AAA2.Text);
+            resultado = moneya - moneyb;
+            Resultado.Text = resultado.ToString();
+        }
+        else
+        {
+            DisplayAlert("Error", "Introduce todos los campos", "ok");
+        }
+    }
+}
--- MenuPrincipal/Menu.xaml.cs
+++ MenuPrincipal/Menu.xaml.cs
@@ -9,6 +9,26 @@ public partial class Menu : ContentPage
 
     private void Button_Clicked_1(object sender, EventArgs e)
     {
+        Navigation.PushModalAsync(new Cambio());
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_3(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_4(object sender, EventArgs e)
+    {
 
     }
 }
